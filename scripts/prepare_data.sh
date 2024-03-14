35b0b749703cecc7f4022b8574ac0dd250f12f15
Aymen Mir <63645632+miraymen@users.noreply.github.com>
2024-03-14 10:54:05 +0100

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O pix2surf.zip https://nextcloud.mpi-klsb.mpg.de/index.php/s/ozRcGdGwAJ3tBns/download
+wget -O pix2surf.zip https://hpsdata.mpi-inf.mpg.de/pix2surf.zip
 unzip pix2surf.zip
 rm pix2surf.zip
 mv data ./train/
